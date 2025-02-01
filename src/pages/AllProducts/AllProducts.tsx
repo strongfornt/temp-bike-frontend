ac36bd9e3a6bc00c199d9e2ce82d59e0a6818845
itsahadul99 <itsahadul999@gmail.com>
2025-02-01 17:12:45 +0600

    refactor product management features and improve user interface

--- src/pages/AllProducts/AllProducts.tsx
+++ src/pages/AllProducts/AllProducts.tsx
@@ -1,84 +1,176 @@
-import { useState } from "react";
-import { Input, Select, Button } from "antd";
+import { Form, Input, Select, Slider } from "antd";
 import ProductCard from "../../components/ProductCard";
 import { useGetProductsQuery } from "../../redux/features/product/productApi";
 
-const { Option } = Select;
 
 const AllProducts = () => {
-    const [query, setQuery] = useState({
-        search: "",
-        priceRange: "",
-        model: "",
-        brand: "",
-        category: "",
-        availability: "",
-    });
 
     // Fetch filtered data from the backend
-    const { data, isLoading } = useGetProductsQuery(query);
+    const { data, isLoading } = useGetProductsQuery(undefined);
     const products = data?.data || [];
-
-    // Handle input changes
-    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setQuery((prev) => ({ ...prev, search: e.target.value }));
-    };
-
-    const handleFilterChange = (name: string, value: string) => {
-        setQuery((prev) => ({ ...prev, [name]: value }));
-    };
+    const [form] = Form.useForm()
 
     return (
         <div className="max-w-7xl mx-auto py-5 text-white">
             {/* Filter Section */}
-            <div className="p-3 rounded-lg shadow mb-6">
+            <div className="rounded-lg shadow mb-6">
                 <h3 className="text-lg font-semibold mb-4">Filters</h3>
+                <Form form={form} className="my-4 flex flex-col md:flex-row justify-between gap-3 md:gap-0 items-start">
+                    <div>
+                        <Form.Item noStyle shouldUpdate>
+                            {() => {
+                                const formData = form.getFieldsValue(true);
+                                return (
+                                    <Input
+                                        placeholder="Search by Brand, Bike Name, or Category"
+                                        value={formData?.searchValue || ''}
+                                        onChange={(event) => {
+                                            const value = event.target.value;
+                                            form.setFieldsValue({ searchValue: value });
+                                        }}
+                                        className="w-full md:w-[180px] py-[6.1px]"
+                                    />
+                                );
+                            }}
+                        </Form.Item>
+                    </div>
+                    <div className="flex flex-col md:flex-row items-center justify-end flex-1 w-full">
+                        <div className="w-full flex md:flex-row flex-wrap flex-col justify-end items-end gap-[10px] transition-all duration-300 opacity-100">
+                            {/* Brand Filter */}
+                            <Form.Item noStyle shouldUpdate>
+                                {() => {
+                                    const formData = form.getFieldsValue(true);
+                                    return (
+                                        <Select
+                                            placeholder="Brand"
+                                            value={formData?.brand || undefined}
+                                            onChange={(key, data: any) => {
+                                                form.setFieldsValue({
+                                                    brand: data?.value,
+                                                });
+                                            }}
+                                            style={{ height: 35, borderRadius: 8, borderColor: '#D7D7D7' }}
+                                            allowClear
+                                            showSearch
+                                            filterOption={(input, option) =>
+                                                (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+                                            }
+                                            className="text-gray-500 md:w-[180px] w-full"
+                                            suffixIcon={<span style={{ color: '#BBBABA' }}>▼</span>}
+                                            options={[{ label: 'Yamaha', value: 'yamaha' }, { label: 'Honda', value: 'honda' }]}
+                                        />
+                                    );
+                                }}
+                            </Form.Item>
 
-                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4">
-                    <Input
-                        placeholder="Search by name, brand, or category"
-                        value={query.search}
-                        onChange={handleSearchChange}
-                    />
+                            {/* Category Filter */}
+                            <Form.Item noStyle shouldUpdate>
+                                {() => {
+                                    const formData = form.getFieldsValue(true);
+                                    return (
+                                        <Select
+                                            placeholder="Category"
+                                            value={formData?.category || undefined}
+                                            onChange={(key, data: any) => {
+                                                form.setFieldsValue({ category: data?.value });
+                                            }}
+                                            style={{ height: 35, borderRadius: 8, borderColor: '#D7D7D7' }}
+                                            allowClear
+                                            showSearch
+                                            filterOption={(input, option) =>
+                                                (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+                                            }
+                                            className="text-gray-500 md:w-[180px] w-full"
+                                            suffixIcon={<span style={{ color: '#BBBABA' }}>▼</span>}
+                                            options={[
+                                                { label: 'Sports', value: 'sports' },
+                                                { label: 'Cruiser', value: 'cruiser' },
+                                            ]}
+                                        />
+                                    );
+                                }}
+                            </Form.Item>
 
-                    <Select
-                        className="w-full"
-                        placeholder="Price Range"
-                        onChange={(value) => handleFilterChange("priceRange", value)}
-                    >
-                        <Option value="">All Prices</Option>
-                        <Option value="0-1000">0 - 1000</Option>
-                        <Option value="1000-5000">1000 - 5000</Option>
-                    </Select>
+                            {/* Model Filter */}
+                            <Form.Item noStyle shouldUpdate>
+                                {() => {
+                                    const formData = form.getFieldsValue(true);
+                                    return (
+                                        <Select
+                                            placeholder="Model"
+                                            value={formData?.model || undefined}
+                                            onChange={(key, data: any) => {
+                                                form.setFieldsValue({ model: data?.value });
+                                            }}
+                                            style={{ height: 35, borderRadius: 8, borderColor: '#D7D7D7' }}
+                                            allowClear
+                                            showSearch
+                                            filterOption={(input, option) =>
+                                                (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+                                            }
+                                            className="text-gray-500 md:w-[180px] w-full"
+                                            suffixIcon={<span style={{ color: '#BBBABA' }}>▼</span>}
+                                            options={[
+                                                { label: 'Model X', value: 'model-x' },
+                                                { label: 'Model Y', value: 'model-y' },
+                                            ]}
+                                        />
+                                    );
+                                }}
+                            </Form.Item>
+                            {/* 
+                            <Form.Item noStyle shouldUpdate>
+                                {() => {
+                                    const formData = form.getFieldsValue(true);
+                                    return (
+                                        <div className="md:w-[200px] w-full text-white">
+                                            <label className=" text-sm mb-1 block">Price Range</label>
+                                            <Slider
+                                                range
+                                                min={500}
+                                                max={5000}
+                                                step={100}
+                                                value={formData?.priceRange || [500, 5000]}
+                                                onChange={(value) => {
+                                                    form.setFieldsValue({ priceRange: value });
+                                                }}
+                                            />
+                                            <div className="flex justify-between  text-xs">
+                                                <span>${formData?.priceRange?.[0] || 500}</span>
+                                                <span>${formData?.priceRange?.[1] || 5000}</span>
+                                            </div>
+                                        </div>
+                                    );
+                                }}
+                            </Form.Item> */}
+                            {/* Availability Filter */}
+                            <Form.Item noStyle shouldUpdate>
+                                {() => {
+                                    const formData = form.getFieldsValue(true);
+                                    return (
+                                        <Select
+                                            placeholder="Availability"
+                                            value={formData?.availability || undefined}
+                                            onChange={(key, data: any) => {
+                                                form.setFieldsValue({ availability: data?.value });
+                                            }}
+                                            style={{ height: 35, borderRadius: 8, borderColor: '#D7D7D7' }}
+                                            allowClear
+                                            showSearch
+                                            className="text-gray-500 md:w-[180px] w-full"
+                                            suffixIcon={<span style={{ color: '#BBBABA' }}>▼</span>}
+                                            options={[
+                                                { label: 'In Stock', value: 'in-stock' },
+                                                { label: 'Out of Stock', value: 'out-of-stock' },
+                                            ]}
+                                        />
+                                    );
+                                }}
+                            </Form.Item>
+                        </div>
+                    </div>
+                </Form>
 
-                    <Select
-                        className="w-full"
-                        placeholder="Brand"
-                        onChange={(value) => handleFilterChange("brand", value)}
-                    >
-                        <Option value="">All Brands</Option>
-                        <Option value="Yamaha">Yamaha</Option>
-                        <Option value="Honda">Honda</Option>
-                    </Select>
-
-                    <Select
-                        className="w-full"
-                        placeholder="Category"
-                        onChange={(value) => handleFilterChange("category", value)}
-                    >
-                        <Option value="">All Categories</Option>
-                        <Option value="Sport">Sport</Option>
-                        <Option value="Cruiser">Cruiser</Option>
-                    </Select>
-
-                    <Button
-                        type="primary"
-                        className="w-full"
-                        onClick={() => console.log("Filtering with:", query)}
-                    >
-                        Apply Filters
-                    </Button>
-                </div>
             </div>
 
             {/* Products Section */}
